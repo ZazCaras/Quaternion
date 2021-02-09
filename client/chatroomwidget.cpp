8fa5db2b8c8ff7ee158fd2a36b65597598b525ba
ZazCaras <ddvallejoj@gmail.com>
2021-02-09 06:53:57 -0600

    a

--- client/chatroomwidget.cpp
+++ client/chatroomwidget.cpp
@@ -19,6 +19,8 @@
 
 #include "chatroomwidget.h"
 
+#include "iostream"
+
 #include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QToolButton>
@@ -648,6 +650,26 @@ QString ChatRoomWidget::sendCommand(const QStringRef& command,
 
 void ChatRoomWidget::sendInput()
 {
+
+    std::string miTexto = m_chatEdit->toPlainText().toStdString();
+    size_t len = miTexto.size();
+    
+
+    std::cout << "Mi texto es: " 
+        << miTexto << " de tamano " 
+        << len << "\n";
+
+    if (len >= 3) {
+        char c1 = miTexto[0];
+        char c2 = miTexto[1];
+        char c3 = miTexto[2];
+"tarea: imprimir cada caracter y su posicion en el string"
+        std::cout << "Caracter 1: " << c1 << "\n";
+        std::cout << "Caracter 2: " << c2 << "\n";
+        std::cout << "Caracter 3: " << c3 << "\n";
+
+    }
+
     if (!attachedFileName.isEmpty())
         sendFile();
     else {
